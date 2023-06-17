7dda324702ab3cbc5f1572e767164cc60ae59789
John Poikilidis <akiskawi@gmail.com>
2023-06-17 23:17:40 +0300

    exerciseController created NOT Tested

--- src/main/java/com/booleanuk/api/controllers/ExerciseController.java
+++ src/main/java/com/booleanuk/api/controllers/ExerciseController.java
@@ -1,4 +1,59 @@
 package com.booleanuk.api.controllers;
 
+import com.booleanuk.api.models.Exercise;
+import com.booleanuk.api.models.Workout;
+import com.booleanuk.api.models.dtos.CreateExercise;
+import com.booleanuk.api.repositories.ExerciseRepository;
+import com.booleanuk.api.services.WorkoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users/{userId}/workouts/{workoutId}/exercises")
 public class ExerciseController {
+    @Autowired
+    WorkoutService workoutService;
+    @Autowired
+    ExerciseRepository exerciseRepository;
+
+    @GetMapping
+    public List<Exercise> getAllExercises(@PathVariable int userId, @PathVariable int workoutId) {
+        return workoutService.getSingleWorkout(userId, workoutId).getExercises();
+    }
+
+    @GetMapping("/{id}")
+    public Exercise getSingleExercise(@PathVariable int userId, @PathVariable int workoutId, @PathVariable int id) {
+        Exercise exercise = exerciseRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Exercise not found"));
+        if (exercise.getWorkout().getId() != workoutId || exercise.getWorkout().getUser().getId() != userId)
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "No user have that workout with that exercise:)");
+        return exercise;
+    }
+
+    @PostMapping
+    public Exercise createExercise(@PathVariable int userId, @PathVariable int workoutId, CreateExercise createExercise) {
+        Workout workout = workoutService.getSingleWorkout(userId, workoutId);
+        return exerciseRepository.save(createExercise.toExercise(workout));
+    }
+    @PutMapping("/{id}")
+    public Exercise updateExercise(@PathVariable int userId, @PathVariable int workoutId,@PathVariable int id, CreateExercise createExercise) {
+       Exercise exercise =getSingleExercise(userId, workoutId, id);
+        if (createExercise.getName()!=null) exercise.setName(createExercise.getName());
+        if (createExercise.getSets()!=0) exercise.setSets(createExercise.getSets());
+        if (createExercise.getReps()!=0) exercise.setReps(createExercise.getReps());
+        if (createExercise.getRPE()!=0) exercise.setRPE(createExercise.getRPE());
+        return exerciseRepository.save(exercise);
+    }
+    @DeleteMapping("/{id}")
+    public Exercise deleteExercise(@PathVariable int userId, @PathVariable int workoutId,@PathVariable int id){
+        Exercise exercise =getSingleExercise(userId, workoutId, id);
+        exerciseRepository.delete(exercise);
+        return exercise;
+    }
+
+
+
 }
--- src/main/java/com/booleanuk/api/repositories/ExerciseRepository.java
+++ src/main/java/com/booleanuk/api/repositories/ExerciseRepository.java
@@ -3,5 +3,7 @@ package com.booleanuk.api.repositories;
 import com.booleanuk.api.models.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExerciseRepository extends JpaRepository<Exercise,Integer> {
 }
